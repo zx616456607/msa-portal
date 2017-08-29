e40e83d1d8daecc7cb8aac55829b39f3bb6dec8a
zhaoyanbei <727114935@qq.com>
2017-08-29 20:04:13 +0800

    commit performance code

--- client/containers/Apm/Performance/index.js
+++ client/containers/Apm/Performance/index.js
@@ -13,17 +13,44 @@
 import React from 'react'
 import './style/index.less'
 import createG2 from 'g2-react'
-import { Row, Icon, Dropdown, Menu, Button, Layout } from 'antd'
+import { connect } from 'react-redux'
+import { setCurrent } from '../../../actions'
+import { Row, Icon, Button, Layout, Select, DatePicker } from 'antd'
 
 const LayoutContent = Layout.Content
-const Chart = createG2(chart => {
+const Option = Select.Option
+const { RangePicker } = DatePicker
+
+let charts
+const Chart1 = createG2(chart => {
+  charts = chart
   chart.line().position('month*temperature').size(2)
   chart.setMode('select')
   chart.select('rangeX')
+  chart.on('plotmove', function(ev) {
+    const point = {
+      x: ev.x,
+      y: ev.y,
+    }
+    charts.showTooltip(point)
+  })
   chart.render()
 })
-
-export default class Performance extends React.Component {
+const Chart = createG2(chart => {
+  charts = chart
+  chart.line().position('month*temperature').size(2)
+  chart.setMode('select')
+  chart.select('rangeX')
+  chart.on('plotmove', function(ev) {
+    const point = {
+      x: ev.x,
+      y: ev.y,
+    }
+    charts.showTooltip(point)
+  })
+  chart.render()
+})
+class Performance extends React.Component {
   state = {
     data: [
       { month: 'Jan', temperature: 7.0 },
@@ -34,35 +61,60 @@ export default class Performance extends React.Component {
     forceFit: true,
     width: 530,
     height: 300,
+    isTimerShow: false,
+  }
+  componentWillMount() {
+
+  }
+  /**
+   * 自定义日期
+   */
+  handleTimer = () => {
+    const { isTimerShow } = this.state
+    if (isTimerShow) {
+      this.setState({
+        isTimerShow: false,
+      })
+    } else {
+      this.setState({
+        isTimerShow: true,
+      })
+    }
   }
 
   render() {
-    const menu = (
-      <Menu>
-        <Menu.Item key="1">第一个菜单项</Menu.Item>
-        <Menu.Item key="2">第二个菜单项</Menu.Item>
-        <Menu.Item key="3">第三个菜单项</Menu.Item>
-      </Menu>
-    )
+    const { isTimerShow } = this.state
     return (
-      <LayoutContent className="Layout-content">
+      <LayoutContent className="content">
         <div className="capability">
           <div className="header">
-            <Dropdown.Button className="server" overlay={menu}>选择微服务</Dropdown.Button>
-            <Dropdown.Button className="Example" overlay={menu}>选择具体实例</Dropdown.Button>
-            <Button className="ref"><Icon type="reload" /> 刷新</Button>
+            <Select className="server" defaultValue="选择微服务" style={{ width: 120 }}>
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+            </Select>
+            <Button className="ref"><Icon type="reload" />刷新</Button>
             <div className="timer">
-              <Button className="btn" ><Icon type="calendar" />自定义日期</Button>
-              <Button className="btn" >最近5分钟</Button>
-              <Button className="btn" >3小时</Button>
-              <Button className="btn" >2小时</Button>
+              <Button className="btn" onClick={() => this.handleTimer()} ><Icon type="calendar" />自定义日期</Button>
+              {
+                isTimerShow ?
+                  <div>
+                    <Button className="btn" >最近5分钟</Button>
+                    <Button className="btn" >3小时</Button>
+                    <Button className="btn" >2小时</Button>
+                  </div> :
+                  <RangePicker className="picker" />
+              }
             </div>
+            <Select className="example" defaultValue="选择具体实例" style={{ width: 120 }}>
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+            </Select>
           </div>
           <Row>
             <div className="section">
               {/* <img src=""/> */}
               <div className="left">
-                <span style={{ fontSize: 20 }}>微服务名称 apmserice</span><br />
+                <span style={{ fontSize: 16 }}>微服务名称 apmserice</span><br />
                 <span>Agent Id 172.18.45.82</span><br />
                 <span>hostname apmservice-2601823123</span><br />
                 <span>IP　172.18.45.82</span><br />
@@ -80,26 +132,36 @@ export default class Performance extends React.Component {
           <Row>
             <div className="footer">
               <div className="left">
-                <div className="titleInfo"><span style={{ color: 'blur', fontSize: 18 }}>Heap Usage</span></div>
+                <div className="titleInfo"><span style={{ color: '#2db7f5', fontSize: 18 }}>Heap Usage 1</span>
+                  <Button className="btn">重置</Button>
+                </div>
                 <Chart
                   data={this.state.data}
                   width={this.state.width}
                   height={this.state.height}
                   forceFit={this.state.forceFit} />
-                <Chart
+                <div className="titleInfo"><span style={{ color: '#2db7f5', fontSize: 18 }}>Heap Usage 2</span>
+                  <Button className="btn">重置</Button>
+                </div>
+                <Chart1
                   data={this.state.data}
                   width={this.state.width}
                   height={this.state.height}
                   forceFit={this.state.forceFit} />
               </div>
               <div className="rigth">
-                <div className="titleInfo"><span style={{ color: 'blur', fontSize: 18 }}>Heap Usage</span></div>
-                <Chart
+                <div className="titleInfo"><span style={{ color: '#2db7f5', fontSize: 18 }}>Heap Usage 3</span>
+                  <Button className="btn">重置</Button>
+                </div>
+                <Chart1
                   data={this.state.data}
                   width={this.state.width}
                   height={this.state.height}
                   forceFit={this.state.forceFit} />
-                <Chart
+                <div className="titleInfo"><span style={{ color: '#2db7f5', fontSize: 18 }}>Heap Usage 4</span>
+                  <Button className="btn">重置</Button>
+                </div>
+                <Chart1
                   data={this.state.data}
                   width={this.state.width}
                   height={this.state.height}
@@ -112,3 +174,13 @@ export default class Performance extends React.Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  errorMessage: state.errorMessage,
+  auth: state.entities.auth,
+  current: state.current || {},
+})
+
+export default connect(mapStateToProps, {
+  setCurrent,
+})(Performance)
