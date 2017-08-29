f340595496b0b8c182027e36a4a3100f34747e8e
zhangpc <zhangpc@tenxcloud.com>
2017-08-29 15:05:18 +0800

    add current user

--- client/actions/current.js
+++ client/actions/current.js
@@ -10,6 +10,8 @@
  * @author zhangpc
  */
 
+import { CALL_API } from '../middleware/api'
+
 export const SET_CURRENT = 'SET_CURRENT'
 
 export function setCurrent(current) {
@@ -17,4 +19,26 @@ export function setCurrent(current) {
     current,
     type: SET_CURRENT,
   }
+}
+
+export const CURRENT_USER_REQUEST = 'CURRENT_USER_REQUEST'
+export const CURRENT_USER_SUCCESS = 'CURRENT_USER_SUCCESS'
+export const CURRENT_USER_FAILURE = 'CURRENT_USER_FAILURE'
+
+// Get current user info.
+// Relies on the custom API middleware defined in ../middleware/api.js.
+const fetchCurrentUser = userID => {
+  return {
+    [CALL_API]: {
+      types: [ CURRENT_USER_REQUEST, CURRENT_USER_SUCCESS, CURRENT_USER_FAILURE ],
+      endpoint: `/users/${userID}`,
+      schema: {},
+    },
+  }
+}
+
+// Fetches current user info.
+// Relies on Redux Thunk middleware.
+export const getCurrentUser = userID => dispatch => {
+  return dispatch(fetchCurrentUser(userID))
 }
