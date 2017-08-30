8fd8772b16038b1063a5e7d7cb82a9652cfade6c
zhangpc <zhangpc@tenxcloud.com>
2017-08-30 15:39:02 +0800

    remove load apm in index page

--- client/containers/IndexPage/index.js
+++ client/containers/IndexPage/index.js
@@ -14,7 +14,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Layout, Menu, Icon } from 'antd'
 import Sider from '../../components/Sider'
-import { loadApms } from '../../actions/apm'
 
 const { Content } = Layout
 
@@ -22,8 +21,7 @@ class IndexPage extends React.Component {
   test = a => <h2>{a}</h2>
 
   componentWillMount() {
-    const { loadApms } = this.props
-    loadApms('test')
+    //
   }
 
   render() {
@@ -80,5 +78,5 @@ const mapStateToProps = state => ({
 })
 
 export default connect(mapStateToProps, {
-  loadApms,
+  //
 })(IndexPage)
