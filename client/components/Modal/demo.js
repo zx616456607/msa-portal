35a83d02ba8a34379dc3c4229a3d23b62409a52c
zhangpc <zhangpc@tenxcloud.com>
2017-11-07 17:27:43 +0800

    add confirm modal component

---
+++ client/components/Modal/demo.js
@@ -0,0 +1,17 @@
+import confirm from './confirm'
+
+function showConfirm() {
+  confirm({
+    modalTitle: 'Confirm delete',
+    message: 'Do you want to delete these items?',
+    content: 'When clicked the OK button, this dialog will be closed after 1 second',
+    onOk() {
+      return new Promise((resolve, reject) => {
+        setTimeout(Math.random() > 0.5 ? resolve : reject, 1000)
+      }).catch(() => console.log('Oops errors!'))
+    },
+    onCancel() {},
+  })
+}
+
+showConfirm()
