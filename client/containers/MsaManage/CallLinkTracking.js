d93e256d6bd908217245c928589b9538284d19a7
zhangpc <zhangpc@tenxcloud.com>
2017-11-14 11:50:37 +0800

    add open in new for CallLinkTracking of msa

--- client/containers/MsaManage/CallLinkTracking.js
+++ client/containers/MsaManage/CallLinkTracking.js
@@ -12,24 +12,34 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card } from 'antd'
+import { Card, Button } from 'antd'
 import QueueAnim from 'rc-queue-anim'
 import { DEFAULT, API_CONFIG } from '../../constants'
 import { toQuerystring } from '../../common/utils'
 import './style/CallLinkTracking.less'
 
+export function getZipkinSrc(currentConfig, currentUser) {
+  const query = {
+    clusterId: currentConfig.cluster.id,
+    namespace: currentUser.namespace,
+  }
+  const iframeSrc = `${API_CONFIG.MSA_API}/zipkin/?${toQuerystring(query)}`
+  return iframeSrc
+}
+
 class CallLinkTracking extends React.Component {
   render() {
     const { currentConfig, currentUser } = this.props
-    const query = {
-      clusterId: currentConfig.cluster.id,
-      namespace: currentUser.namespace,
-    }
-    const iframeSrc = `${API_CONFIG.MSA_API}/zipkin/?${toQuerystring(query)}`
+    const iframeSrc = getZipkinSrc(currentConfig, currentUser)
     return (
       <QueueAnim className="msa-call-link-tracking">
+        <div className="router-manage-btn-box layout-content-btns" key="btns">
+          <Button onClick={() => window.open(iframeSrc)} type="dashed" icon="export">
+          新窗口打开
+          </Button>
+        </div>
         <div className="layout-content-body" key="body">
-          <Card>
+          <Card noHovering>
             <iframe src={iframeSrc} />
           </Card>
         </div>
