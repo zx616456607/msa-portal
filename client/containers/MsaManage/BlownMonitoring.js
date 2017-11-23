cf6a4907512ad289174dcf3f3d881802cdaf0a6f
zhangpc <zhangpc@tenxcloud.com>
2017-11-23 17:50:08 +0800

    get the host of blown monitoring from cluster

--- client/containers/MsaManage/BlownMonitoring.js
+++ client/containers/MsaManage/BlownMonitoring.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import QueueAnim from 'rc-queue-anim'
 import { Card, Icon } from 'antd'
 import './style/BlownMonitoring.less'
 
-export default class BlownMonitoring extends React.Component {
+class BlownMonitoring extends React.Component {
   render() {
-    const iframeSrc = 'http://192.168.1.249:9901/hystrix/monitor?stream=http://192.168.1.249:9902/trubine.stream'
+    const { currentCluster } = this.props
+    const { apiHost } = currentCluster
+    const blownUrl = `http://${apiHost.split(':')[0]}`
+    const iframeSrc = `${blownUrl}:9901/hystrix/monitor?stream=${blownUrl}:9902/trubine.stream`
     const extra = <a target="_blank" href={iframeSrc}>
       <Icon type="export" /> 新页签打开
     </a>
@@ -20,3 +24,15 @@ export default class BlownMonitoring extends React.Component {
     )
   }
 }
+
+const mapStateToProps = state => {
+  const { current, entities } = state
+  const currentCluster = entities.clusters[current.config.cluster.id]
+  return {
+    currentCluster,
+  }
+}
+
+export default connect(mapStateToProps, {
+  //
+})(BlownMonitoring)
