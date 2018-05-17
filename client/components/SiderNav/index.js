b114ef15563b38c8382a0a9c3102c2d2e01da1f1
Songsz <shangzhisong@gmail.com>
2018-05-17 21:21:48 +0800

    backup

--- client/components/SiderNav/index.js
+++ client/components/SiderNav/index.js
@@ -11,18 +11,84 @@
  */
 import React from 'react'
 import { Layout, Menu, Icon } from 'antd'
-// import styles from './style/index.less'
+import './style/index.less'
 import logo from '../../assets/img/logo.svg'
+import globalSetting from '../../assets/img/system-settings/global-setting.svg'
+import msaconfig from '../../assets/img/msa-manage/msa.svg'
+import topologyIcon from '../../assets/img/apm/apm.svg'
+import msaComponent from '../../assets/img/msa-om/msa-component.svg'
+import csbInstancesOm from '../../assets/img/msa-om/csb-instances-om.svg'
 
 const { Sider } = Layout
-// const SubMenu = Menu.SubMenu
+const SubMenu = Menu.SubMenu
 
+// const menus = [
+//   {
+//     path: '/cluster',
+//     name: '微服务运维',
+//     icon: 'appstore',
+//     children: [{
+//       path: '/cluster',
+//       name: '微服务组件',
+//       icon: msaComponent,
+//     }, {
+//       path: '/cluster',
+//       name: 'CSB 运维',
+//       icon: csbInstancesOm,
+//       children: [
+//         {
+//           path: '/cluster',
+//           name: '实例列表',
+//         }, {
+//           path: '/cluster',
+//           name: '实例审批',
+//         }, {
+//           path: '/cluster',
+//           name: '级联链路规则',
+//         },
+//       ],
+//     }, {
+//       path: '/cluster',
+//       name: '日志查询',
+//       icon: 'file-text',
+//     }],
+//   },
+// ]
+const svgIcon = icon => (
+  <svg className="itemIcon">
+    <use xlinkHref={`#${icon.id}`} />
+  </svg>
+)
 class SiderNav extends React.Component {
   state = {
     collapsed: false,
   };
   onCollapse = collapsed => {
     this.setState({ collapsed })
+  }
+  renderMenuItem = data => {
+    const { children, name, icon } = data
+    if (children) {
+      return (
+        <SubMenu
+          key="sub1"
+          title={<span>
+            { icon && (typeof icon === 'object') && svgIcon(icon) }
+            {
+              icon && (typeof icon === 'string') &&
+              <Icon type={icon} />
+            }
+            <span>{name}</span>
+          </span>}>
+          {
+            children.map(item => this.renderMenuItem(item))
+          }
+        </SubMenu>
+      )
+    }
+    return (
+      <Menu.Item key="3">{name}</Menu.Item>
+    )
   }
   render() {
     return (
@@ -31,22 +97,52 @@ class SiderNav extends React.Component {
         collapsible
         collapsed={this.state.collapsed}
       >
-        <svg className="logo">
+        <svg className={'logo'}>
           <use xlinkHref={`#${logo.id}`} />
         </svg>
         <Menu theme="dark" mode="inline" defaultSelectedKeys={[ '1' ]}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span>nav 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span>nav 2</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="upload" />
-            <span>nav 3</span>
-          </Menu.Item>
+          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>微服务运维</span></span>}>
+            <Menu.Item key="5">
+              <svg className="itemIcon">
+                <use xlinkHref={`#${msaComponent.id}`} />
+              </svg>
+              微服务组件</Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span>
+                <svg className="itemIcon">
+                  <use xlinkHref={`#${csbInstancesOm.id}`} />
+                </svg>
+                <span>CSB 运维</span>
+              </span>}>
+              <Menu.Item key="3">实例列表</Menu.Item>
+              <Menu.Item key="4">实例审批</Menu.Item>
+              <Menu.Item key="66">级联链路规则</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="5">
+              <Icon type="file-text" style={{ fontSize: 15 }} />
+              日志查询</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub4" title={<span><Icon type="setting" /><span>系统设置</span></span>}>
+            <Menu.Item key="9">
+              <svg className="itemIcon">
+                <use xlinkHref={`#${globalSetting.id}`} />
+              </svg>
+              全局配置</Menu.Item>
+            <Menu.Item key="10">
+              <svg className="itemIcon">
+                <use xlinkHref={`#${msaconfig.id}`} />
+              </svg>
+              微服务配置</Menu.Item>
+            <Menu.Item key="11">
+              <svg className="itemIcon">
+                <use xlinkHref={`#${topologyIcon.id}`} />
+              </svg>
+              APM 配置</Menu.Item>
+          </SubMenu>
+          {
+            // menus.map(item => this.renderMenuItem(item))
+          }
         </Menu>
       </Sider>
     )
