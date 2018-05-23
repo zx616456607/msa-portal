eb5dfe462cf00fa1757f65411fbe044d9f0b3731
Songsz <shangzhisong@gmail.com>
2018-05-23 11:49:17 +0800

    add div to prevent name move left when children is hidden, in  header

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -84,6 +84,8 @@ export default class Header extends React.Component {
     return (
       <LayoutHeader className={containerStyles}>
         {children}
+        {/* <div/>作为占位符, 当children不存在时, 防止name跑到左侧 */}
+        <div/>
         <div className="">
           <Dropdown
             overlay={
