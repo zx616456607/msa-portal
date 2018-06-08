8e562d1f3e3f3f2febe938aee4ef4289ee8fc2cb
zhangxuan <zhangxuan@tenxcloud.com>
2018-06-08 21:34:46 +0800

    fix blownMonitoring page empty

--- client/containers/MsaManage/BlownMonitoring.js
+++ client/containers/MsaManage/BlownMonitoring.js
@@ -3,13 +3,24 @@ import { connect } from 'react-redux'
 import QueueAnim from 'rc-queue-anim'
 import { Card, Icon } from 'antd'
 import './style/BlownMonitoring.less'
+import { getServiceDetail, getClusterProxies } from '../../actions/msa'
+
+const DEFAULT_SERVICE = 'spring-cloud-hystrix-turbine'
 
 class BlownMonitoring extends React.Component {
+  componentDidMount() {
+    const { getServiceDetail, currentCluster, getClusterProxies } = this.props
+    const { clusterID } = currentCluster
+    getServiceDetail(clusterID, DEFAULT_SERVICE)
+    getClusterProxies(clusterID)
+  }
+
   render() {
-    const { currentCluster } = this.props
-    const { apiHost } = currentCluster
-    const blownUrl = `http://${apiHost.split(':')[0]}`
-    const iframeSrc = `${blownUrl}:9901/hystrix/monitor?stream=${blownUrl}:9902/trubine.stream`
+    // const { currentCluster } = this.props
+    // const { apiHost } = currentCluster
+    // const blownUrl = `http://${apiHost.split(':')[0]}`
+    // const iframeSrc = `${blownUrl}:9901/hystrix/monitor?stream=${blownUrl}:9902/trubine.stream`
+    const iframeSrc = 'http://192.168.1.230:37037/hystrix/monitor?stream=http://192.168.1.230:11619/trubine.stream'
     const extra = <a target="_blank" href={iframeSrc}>
       <Icon type="export" /> 新页签打开
     </a>
@@ -34,5 +45,6 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, {
-  //
+  getServiceDetail,
+  getClusterProxies,
 })(BlownMonitoring)
