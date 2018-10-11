7992458b879a920dc278cab9a0e58c7edc7bd50e
zhouhaitao <capslock.zhou@tenxcloud.com>
2018-10-11 18:26:56 +0800

    @newFeature LOT-2478 策略定义前端实现（路由规则） 页面搭建

--- client/containers/ServiceMesh/RoutesManagement/NewRoute.js
+++ client/containers/ServiceMesh/RoutesManagement/NewRoute.js
@@ -12,7 +12,9 @@
 
 import React from 'react'
 import ReturnButton from '@tenx-ui/return-button'
-import { Card, Form, Input, Select, Radio, Button, Tag } from 'antd'
+import { Card, Form, Input, Select, Radio, Button, Tag, Icon, Tooltip } from 'antd'
+import { connect } from 'react-redux'
+import { getMeshGateway } from '../../../actions/meshGateway'
 import './style/NewRoute.less'
 
 const FormItem = Form.Item
@@ -28,11 +30,30 @@ const formItemLayout = {
     sm: { span: 10, push: 1 },
   },
 }
-
+let hostUuid = 1;
+let routeUuid = 10000;
+const mapStateToProps = state => {
+  return {
+    meshGatewayList: state.meshGateway.meshGatewayList,
+    clusterID: state.current && state.current.config.cluster.id,
+  }
+}
+@connect(mapStateToProps, {
+  getMeshGateway,
+})
 class NewRouteComponent extends React.Component {
   state = {
     ruleName: '',
     visitType: 'pub',
+    routeType: 'content',
+    gateways: [],
+    hosts: [
+      {},
+    ],
+  }
+  componentDidMount() {
+    const { clusterID, getMeshGateway } = this.props
+    getMeshGateway(clusterID)
   }
   nameProps = () => {
     const { getFieldDecorator } = this.props.form
@@ -44,16 +65,197 @@ class NewRouteComponent extends React.Component {
       trigger: 'onChange',
     })
   }
+  domainProps = () => {
+    const { getFieldDecorator } = this.props.form
+    return getFieldDecorator('serviceDomain', {
+      rules: [
+        { required: true, message: '请输入服务域名' },
+      ],
+      trigger: 'onChange',
+    })
+  }
   visitTypeProps = () => {
     const { getFieldDecorator } = this.props.form
     return getFieldDecorator('visitType', {
       initialValue: this.state.visitType,
       onChange: e => this.setState({ visitType: e.target.value }),
     })
-
   }
+  renderHosts = () => {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 2 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 10, push: 1 },
+      },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 20, offset: 3 },
+      },
+    };
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    const remove = k => {
+      const { form } = this.props;
+      // can use data-binding to get
+      const keys = form.getFieldValue('hostKeys');
+      // We need at least one passenger
+      if (keys.length === 1) {
+        return;
+      }
+
+      // can use data-binding to set
+      form.setFieldsValue({
+        hostKeys: keys.filter(key => key !== k),
+      });
+    }
+    const add = () => {
+      const { form } = this.props;
+      const keys = form.getFieldValue('hostKeys');
+      const nextKeys = keys.concat(hostUuid);
+      hostUuid++;
+      form.setFieldsValue({
+        hostKeys: nextKeys,
+      });
+    }
+    getFieldDecorator('hostKeys', { initialValue: [ 0 ] });
+    const keys = getFieldValue('hostKeys');
+    const hostItems = keys.map((k, index) => {
+      return (
+        <FormItem
+          {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+          label={index === 0 ? <span>服务域名
+            <Tooltip title= "服务对外访问地址，用户需自行申请，并确保所填域名能解析到所选网关的服务地址（在选择的网关中添加该域名）">
+              <Icon style={{ marginLeft: 8 }} type="question-circle" theme="outlined" />
+            </Tooltip>
+          </span> : ''}
+          required={false}
+          key={k}
+        >
+          {getFieldDecorator(`names[${k}]`, {
+            validateTrigger: [ 'onChange', 'onBlur' ],
+            rules: [{
+              required: true,
+              whitespace: true,
+              message: '请输入服务域名',
+            }],
+          })(
+            <Input
+              addonAfter={<Icon type="plus" onClick={() => add()}/>}
+              placeholder="passenger name"
+              style={{ width: '200px', marginRight: 8 }} />
+          )}
+          {keys.length > 1 ? (
+            <Button
+              icon="minus"
+              style={{ marginLeft: 16 }}
+              disabled={keys.length === 1}
+              onClick={() => remove(k)}
+            />
+          ) : null}
+        </FormItem>
+      );
+    });
+    return hostItems
+  }
+  renderRouteItem = () => {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 2 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 10, push: 1 },
+      },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 20, offset: 3 },
+      },
+    };
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    const remove = k => {
+      const { form } = this.props;
+      // can use data-binding to get
+      const keys = form.getFieldValue('routeKeys');
+      // We need at least one passenger
+      if (keys.length === 1) {
+        return;
+      }
+
+      // can use data-binding to set
+      form.setFieldsValue({
+        routeKeys: keys.filter(key => key !== k),
+      });
+    }
+    const add = () => {
+      const { form } = this.props;
+      const keys = form.getFieldValue('routeKeys');
+      const nextKeys = keys.concat(routeUuid);
+      routeUuid++;
+      form.setFieldsValue({
+        routeKeys: nextKeys,
+      });
+    }
+    getFieldDecorator('routeKeys', { initialValue: [ 9999 ] });
+    const keys = getFieldValue('routeKeys');
+    const routesBasedReqContent = keys.map((k, index) => {
+      return (
+        <FormItem
+          {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+          label={index === 0 ? '路由类型' : ''}
+          required={false}
+          key={k}
+        >
+          {
+            index === 0 && <div>
+              <RadioGroup
+                value={this.state.routeType}
+                onChange={e => this.setState({ routeType: e.target.value })}>
+                <Radio value="content">基于请求内容</Radio>
+                <Radio value="traffic" disabled>基于流量比例</Radio>
+              </RadioGroup>
+            </div>
+          }
+          {getFieldDecorator(`names[${k}]`, {
+            validateTrigger: [ 'onChange', 'onBlur' ],
+            rules: [{
+              required: true,
+              whitespace: true,
+              message: '请输入服务域名',
+            }],
+          })(
+            <Input
+              addonAfter={<Icon type="plus" onClick={() => add()}/>}
+              placeholder="passenger name"
+              style={{ width: '200px', marginRight: 8 }} />
+          )}
+          {keys.length > 1 ? (
+            <Button
+              icon="minus"
+              style={{ marginLeft: 16 }}
+              disabled={keys.length === 1}
+              onClick={() => remove(k)}
+            />
+          ) : null}
+        </FormItem>
+      );
+    });
+    return <div className="route-rules-wrapper">
+      {this.state.routeType === 'content' ? routesBasedReqContent : ''}
+    </div>
+  }
+
   render() {
+    const { meshGatewayList } = this.props
     const { nameProps, visitTypeProps } = this
+    const { gateways } = this.state
     const { visitType } = this.state
     return <div id="new-route">
       <div className="top">
@@ -107,13 +309,17 @@ class NewRouteComponent extends React.Component {
                         placeholder="选择网关（gateway）"
                         style={{ width: 200 }}
                       >
-                        <Option value="gateway1">gateway1</Option>
-                        <Option value="gateway2">gateway2</Option>
+                        {
+                          (meshGatewayList.data && meshGatewayList.data.length !== 0) &&
+                            meshGatewayList.data.map(v => <Option value={v} key={v}>{v}</Option>)
+                        }
                       </Select>
                       <Button icon="sync"/>
-                      <a href="/www.baidu.com" target="_blank">去创建网关 >></a>
+                      <a href="/service-mesh/mesh-gateway" target="_blank">去创建网关 >></a>
                       <div>
-                        <Tag closable>网关1</Tag><Tag closable>网关2</Tag>
+                        {
+                          gateways && gateways.map(v => <Tag closable key={ v.id }>{v.text}</Tag>)
+                        }
                       </div>
                     </div>
                   </div>
@@ -124,10 +330,13 @@ class NewRouteComponent extends React.Component {
               }
             </div>
           </FormItem>
+          {this.renderHosts()}
+          {this.renderRouteItem()}
         </Form>
       </Card>
     </div>
   }
 }
+
 const NewRoute = Form.create()(NewRouteComponent)
 export default NewRoute
