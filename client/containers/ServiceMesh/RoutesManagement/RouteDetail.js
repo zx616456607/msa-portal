84a418a9c0cd932788e3662891b586f501051d61
zhouhaitao <capslock.zhou@tenxcloud.com>
2018-10-19 19:38:52 +0800

    @fixed issue Service mesh 路由规则部分体验问题

--- client/containers/ServiceMesh/RoutesManagement/RouteDetail.js
+++ client/containers/ServiceMesh/RoutesManagement/RouteDetail.js
@@ -12,13 +12,16 @@
 
 import React from 'react'
 import ReturnButton from '@tenx-ui/return-button'
-import { Card, Form, Input, Select, Radio, Button, Tag, Icon, Tooltip, notification, Checkbox } from 'antd'
+import { Card, Form, Input, Select, Row, Col, Radio, Button, Tag, Icon, Tooltip, notification, Checkbox } from 'antd'
 import { connect } from 'react-redux'
 import { getMeshGateway } from '../../../actions/meshGateway'
 import { loadComponent } from '../../../actions/serviceMesh'
 import { createNewRoute, getMeshRouteDetail, updateMeshRoute } from '../../../actions/meshRouteManagement'
 import './style/NewRoute.less'
-import { MESH_ROUTE_RULE_NAME_REG, MESH_ROUTE_RULE_NAME_REG_NOTICE } from '../../../constants';
+import { MESH_ROUTE_RULE_NAME_REG,
+  MESH_ROUTE_RULE_NAME_REG_NOTICE,
+  IP_REG,
+  ROUTE_REG } from '../../../constants';
 
 const FormItem = Form.Item
 const Option = Select.Option
@@ -34,6 +37,14 @@ const formItemLayout = {
     sm: { span: 10, push: 1 },
   },
 }
+const routeItemInnerLayout = {
+  labelCol: {
+    sm: { span: 2 },
+  },
+  wrapperCol: {
+    sm: { span: 20, push: 1 },
+  },
+};
 const dynamicFormItemLayout = {
   labelCol: {
     xs: { span: 24 },
@@ -217,7 +228,32 @@ class NewRouteComponent extends React.Component {
       // onChange: e => this.setState({ visitType: e.target.value }),
     })
   }
-
+  domainValidator = (rule, value, cb) => {
+    if (!value) return cb()
+    const domainReg = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z0-9]{1,}\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+    if (domainReg.test(value) || IP_REG.test(value)) {
+      return cb()
+    }
+    cb('请填写正确的服务域名')
+  }
+  ruleOfRouteValidator = (rule, value, cb) => {
+    const values = this.props.form.getFieldsValue()
+    const routeRules = values.rule.filter(v => typeof v === 'string')
+    if (routeRules.length > 1) {
+      const existRouteRules = routeRules.slice(0, routeRules.length - 1)
+      if (existRouteRules.includes(routeRules[routeRules.length - 1])) {
+        cb('路由规则不能重复')
+      }
+    }
+    if (!value) cb('请填写路由规则')
+    if (!ROUTE_REG.test(value)) cb('请填写正确的路由规则')
+    cb()
+  }
+  versionValidator = (rule, value, cb) => {
+    if (value.length === 0) {
+      cb('请选择作用版本')
+    }
+  }
   // 页面渲染相关
   addItem = (key, thisKey) => {
     const { form } = this.props;
@@ -295,6 +331,8 @@ class NewRouteComponent extends React.Component {
               required: true,
               whitespace: true,
               message: '请输入服务域名',
+            }, {
+              validator: this.domainValidator,
             }],
           })(
             <Input
@@ -330,14 +368,6 @@ class NewRouteComponent extends React.Component {
     const routeConditionsItem = uniqueKey => {
       getFieldDecorator(`routeConditionkeys[${uniqueKey}]`, { initialValue: [ uniqueKey ] });
       const routeConditionList = getFieldValue(`routeConditionkeys[${uniqueKey}]`);
-      const conditionFormItemLayout = {
-        labelCol: {
-          sm: { span: 2 },
-        },
-        wrapperCol: {
-          sm: { span: 20, push: 1 },
-        },
-      };
       const conditionFormItemLayoutWithoutLabel = {
         wrapperCol: {
           sm: { span: 10, offset: 3 },
@@ -345,73 +375,78 @@ class NewRouteComponent extends React.Component {
       };
       const routeConditionsDomList = routeConditionList.map((k, index) => {
         return (
-          <FormItem
-            label= { index === 0 ? '路由条件' : ''}
-            key={k}
-            {...(index === 0 ? conditionFormItemLayout : conditionFormItemLayoutWithoutLabel)}
-          >
-            <div className="condition-item">
-              <div>
-                { // 匹配规则暂时隐藏
-                  false &&
+          <div className="route-item">
+            <FormItem
+              label= { index === 0 ? '路由条件' : ''}
+              key={k}
+              {...(index === 0 ? routeItemInnerLayout : conditionFormItemLayoutWithoutLabel)}
+            >
+              <div className="condition-item">
+                <div>
+                  { // 匹配规则暂时隐藏
+                    false &&
+                    <FormItem>
+                      {
+                        getFieldDecorator(`matchValue[${k}]`, {
+                          initialValue: 'all',
+                        })(
+                          <RadioGroup>
+                            <Radio value="all">全部</Radio>
+                            <Radio value="any">任何</Radio>
+                          </RadioGroup>
+                        )
+                      }
+                      <span>以下条件</span>
+                    </FormItem>
+                  }
+                </div>
+                <div className="condition-input">
                   <FormItem>
-                    {
-                      getFieldDecorator(`matchValue[${k}]`, {
-                        initialValue: 'all',
-                      })(
-                        <RadioGroup>
-                          <Radio value="all">全部</Radio>
-                          <Radio value="any">任何</Radio>
-                        </RadioGroup>
-                      )
-                    }
-                    <span>以下条件</span>
+                    {getFieldDecorator(`condition[${k}]`, {
+                      initialValue: 'uri',
+                    })(
+                      <Select
+                        style={{ width: 100 }}
+                      >
+                        {/* <Option value="Header">Header</Option>*/}
+                        <Option value="uri">uri</Option>
+                      </Select>
+                    )}
                   </FormItem>
-                }
-              </div>
-              <div className="condition-input">
-                <FormItem>
-                  {getFieldDecorator(`condition[${k}]`, {
-                    initialValue: 'uri',
-                  })(
-                    <Select
-                      style={{ width: 100 }}
-                    >
-                      {/* <Option value="Header">Header</Option>*/}
-                      <Option value="uri">uri</Option>
-                    </Select>
-                  )}
-                </FormItem>
-                <FormItem>
-                  {getFieldDecorator(`matchRule[${k}]`, {
-                    initialValue: name ? (routeConditionkeys[uniqueKey] && routeConditionkeys[uniqueKey].matchRule) : 'prefix',
-                  })(
-                    <Select
-                      style={{ width: 100 }}
-                    >
-                      <Option value="prefix">前缀匹配</Option>
-                      <Option value="exact">完全匹配</Option>
-                    </Select>
-                  )}
-                </FormItem>
+                  <FormItem>
+                    {getFieldDecorator(`matchRule[${k}]`, {
+                      initialValue: name ? (routeConditionkeys[uniqueKey] && routeConditionkeys[uniqueKey].matchRule) : 'prefix',
+                    })(
+                      <Select
+                        style={{ width: 100 }}
+                      >
+                        <Option value="prefix">前缀匹配</Option>
+                        <Option value="exact">完全匹配</Option>
+                      </Select>
+                    )}
+                  </FormItem>
 
-                <FormItem>
-                  {getFieldDecorator(`rule[${k}]`, {
-                    initialValue: routeConditionkeys[uniqueKey] &&
-                    routeConditionkeys[uniqueKey].rule,
-                  })(
-                    <Input style={{ width: 100 }} placeholder="请输入规则"/>
-                  )}
-                </FormItem>
-                {/* 暂时不需要添加和删除按钮
+                  <FormItem>
+                    {getFieldDecorator(`rule[${k}]`, {
+                      initialValue: routeConditionkeys[uniqueKey] &&
+                      routeConditionkeys[uniqueKey].rule,
+                      validateTrigger: [ 'onChange', 'onBlur' ],
+                      rules: [{ validator: this.ruleOfRouteValidator }],
+                    })(
+                      <Input style={{ width: 100 }} placeholder="请输入规则"/>
+                    )}
+                  </FormItem>
+                  {/* 暂时不需要添加和删除按钮
                 <Button icon="plus" onClick={() => {
                   this.addItem('routeConditionkeys', uniqueKey)
                 }}/>
                 <Button type="danger" icon="close" onClick={() => this.removeItem(k, `routeConditionkeys[${uniqueKey}]`)} />
                 */}
+                </div>
               </div>
-            </div>
-          </FormItem>
+            </FormItem>
+
+          </div>
         )
       })
       return routeConditionsDomList
@@ -432,14 +467,14 @@ class NewRouteComponent extends React.Component {
                 <Radio value="content">基于请求内容</Radio>
                 <Radio value="traffic" disabled>基于流量比例</Radio>
               </RadioGroup>
-              <div className="route-type-tip">
+              {/* <div className="route-type-tip">
                 <span>
                   <Icon type="question"/>
                   选择“基于请求内容类型”，您以前配置的【基于流量比例规则】将全部删除
                 </span>
                 <Icon type="close"/>
-              </div>
-              添加路由项
+              </div>*/}
+              <div>添加路由项</div>
             </div>
           }
           <div className="route-type-item">
@@ -448,19 +483,28 @@ class NewRouteComponent extends React.Component {
             </div>
 
             <div>{routeConditionsItem(k)}</div>
-            <FormItem
-              label="作用版本"
-              {...dynamicFormItemLayout}
-            >
-              {actionVersionOptions.length === 0 ? '请选择组件' : getFieldDecorator(`version[${k}]`, {
-                initialValue: routeConditionkeys[k] &&
-                routeConditionkeys[k].version,
-              })(<CheckboxGroup options={actionVersionOptions}/>)}
-              <div className="route-item-tip">
-                当HTTP Header“User-Agent”包含“Chrome”且“User-Agent”包含“Nexus 6P”时，
-                所有请求将被发送到 ["1.0.1（service1）"]
-              </div>
-            </FormItem>
+            <div className="action-version">
+              <FormItem
+                label="作用版本"
+                {...routeItemInnerLayout}
+              >
+                {actionVersionOptions.length === 0 ? '请选择组件' : getFieldDecorator(`version[${k}]`, {
+                  initialValue: routeConditionkeys[k] &&
+                  routeConditionkeys[k].version,
+                  validateTrigger: [ 'onChange' ],
+                  rules: [{ validator: this.versionValidator }],
+                })(<CheckboxGroup options={actionVersionOptions}/>)}
+              </FormItem>
+            </div>
+            <Row>
+              <Col span={3}></Col>
+              <Col span={20}>
+                <div className="route-item-tip">
+                  当HTTP Header“User-Agent”包含“Chrome”且“User-Agent”包含“Nexus 6P”时，
+                  所有请求将被发送到 ["1.0.1（service1）"]
+                </div>
+              </Col>
+            </Row>
           </div>
           {
             index === routeList.length - 1 && <div className="add-new-route" onClick={() => this.addItem('routeKeys')}><Icon type="plus-circle"/>添加一个路由项</div>
@@ -690,7 +734,7 @@ class NewRouteComponent extends React.Component {
               }
             </div>
           </FormItem>
-          {this.renderHosts()}
+          {this.state.visitType === 'pub' && this.renderHosts()}
           {this.renderRouteItem()}
 
         </Form>
